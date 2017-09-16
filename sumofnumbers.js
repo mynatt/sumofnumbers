827d4b6a0997685cf28ffcb6a08ccc09837ba316
micah <micah@micahs-MacBook-Pro.local>
2017-09-16 12:28:16 -1000

    done-zo

---
+++ sumofnumbers.js
@@ -0,0 +1,36 @@
+function sumFor(arr) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+	return sum;
+}
+function sumWhile(arr) {
+	let i = arr.length - 1, sum = 0;
+	while (i >= 0)
+	{
+		sum += arr[i];
+		i--;
+	}
+	return sum;
+}
+
+function sumRecursive(arr, i = 0) {
+	if (i < arr.length) {
+		return arr[i] + sumRecursive(arr, i+1);
+	}
+	else {
+		return 0;
+	}
+}
+
+function sumTheSimpleWay(arr) {
+	return _.reduce(arr, function(a,b) { return a + b; }, 0);
+}
+
+
+let arr = [1, 2, 3, 4];
+console.log(sumFor(arr));
+console.log(sumWhile(arr));
+console.log(sumRecursive(arr));
+console.log(sumTheSimpleWay(arr));
